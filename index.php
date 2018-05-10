b3ce1f1b7a28a31a7cc5eaaef5c952508f7a481d
shani kumar <shani.pathak98@gmail.com>
2018-05-11 02:18:27 +0530

    my portfolio!..

---
+++ index.php
@@ -0,0 +1,344 @@
+<html lang="em">
+<head>
+<title>Shani Pathak</title>
+<link rel="shortcut icon" href="favicon1.jpg">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#2C3E50"/>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-freelancer/3.3.7/css/freelancer.min.css">
+
+<style>
+@media screen and (max-width:600px){
+span{
+  font-size: 22px;
+  color: #ffffff;}
+}
+a.nav-link:hover
+{
+  background-color: #2c3e50;
+text-decoration: underline;
+}
+.nav-item>a{
+font-family: "Raleway";
+color: #ccc;
+letter-spacing: 2pt;
+}
+a.nav-link:hover{
+  background-color: #ffffff;
+  text-decoration: none;
+  border-radius: 10px;
+  color: #00FF00;
+}
+a.nav-link:active
+{
+  background-color: #18bc9c;
+  border-radius: 10px;
+  color: red;
+}
+a.nav-link:focus
+{ color: red;
+  background-color: #00FF00;
+  border-radius: 10px;
+}
+.center-block {
+  display: block;
+  margin-right: auto;
+  margin-left: auto;
+}
+h5.a:hover
+{
+  background-color: #ffffff;
+  color: red;
+}
+
+</style>
+</head>
+<body class="index" id="page-top">
+<!--navbar-light-->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M43TMD7"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <nav class="navbar navbar-expand-lg navbar-light" id="mainNav" style="background-color:#2C3E50;padding:20px;" class="nav-collapse collapse">
+          <a class="navbar-brand navbar-default navbar-inverse" href="#page-top" style="font-size:25px"><span style="color:#ffffff;">SHANI </span><span style="color: #18BC9C"> KUMAR</span>
+            <span style="color:#ffffff"> PATHAK</span></a>
+          <button class="navbar-toggler navbar-toggler-right" style="color:#ffffff;border:#ffffff" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+          aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fa fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive" style="font-size:20px">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#" style="color:#ccc">Portfolio</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#skills" style="color:#ccc;">Skills</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Projects" style="color:#ccc;">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about" style="color:#ccc">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact" style="color:#ccc">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+<section id="#" style="background-color:#18BC9C">
+     <div class="container">
+      <div class="row">
+         <div class="col-lg-12 col-sm-12 ml-auto"><br><br><br><br><br>
+           <img class="d-block img-fluid ml-auto mr-auto img-thumbnail rounded-circle"  width="200px;" src="skp.jpg" alt="pk">
+           <h2 class="text-center" style="font-size:60px;color:ffffff">H! I M Shani Pathak</h2>
+
+          <h2 class="text-center" style="color:ffffff;padding:0px;">
+            <i class="fa fa-minus" aria-hidden="true"></i>
+            <i class="fa fa-minus" aria-hidden="true"></i>
+            <i class="fa fa-minus" aria-hidden="true"></i>
+            <i class="fa fa-minus" aria-hidden="true"></i>
+      <i class="fa fa-star" aria-hidden="true"></i>
+           <i class="fa fa-minus" aria-hidden="true"></i>
+           <i class="fa fa-minus" aria-hidden="true"></i>
+           <i class="fa fa-minus" aria-hidden="true"></i>
+           <i class="fa fa-minus" aria-hidden="true"></i>
+         </h2>
+         <h2 class="text-center" style="font-size:40px;color:ffffff">IT Student</h2>
+         <br><br><br><br><br><br><br><br>
+         </div>
+         </div>
+     </div>
+   </section>
+
+
+<section id="about" style="background-color:000000;color:ffffff">
+     <div class="container">
+       <h2 class="text-center" style="font-size:60px;">ABOUT</h2>
+       <hr class="star-light">
+      <div class="row">
+         <div class="col-lg-4 ml-auto">
+          <p>I'M Currently Under Graduate In <br>B.tech Information Technology <br>At Cochin University Of <br>Science And Technology          </p>
+         </div>
+         <div class="col-lg-4 mr-auto">
+
+           <p>| Programmer<br>| Web Developer<br> | #Code Python<br> | Beginner in Android Dev.
+           </p>
+         </div>
+       </div>
+     </div>
+   </section>
+   
+
+   <!-- Skills Section -->
+      <section class="success" id="skills" style="background-color:#32CD32;">
+        <div class="container">
+          <h2 class="text-center">Skills</h2>
+          <hr class="star-light">
+          <div class="row">
+            <div class="col-lg-2 ml-auto">
+              <div class="row">
+                <div class="col-lg-4">
+                  <i class="devicon-c-plain big-icon font-cog"></i>
+                </div>
+                <div class="col-lg-4">
+                  <b>C/C++</b>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mr-auto">
+              <div class="progress progress-bar-custom-back">
+                <div class="progress-bar progress-bar-striped active progress-bar-custom" role="progressbar"
+                aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:65%;height:30px;">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="row">
+            <div class="col-lg-2 ml-auto">
+              <div class="row">
+                <div class="col-lg-4">
+                  <i class="devicon-java-plain big-icon font-cog"></i>
+                </div>
+                <div class="col-lg-4">
+                  <b>JAVA</b>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mr-auto">
+              <div class="progress progress-bar-custom-back">
+                <div class="progress-bar progress-bar-striped active progress-bar-custom" role="progressbar"
+                aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%;height:30px;">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+
+          <div class="row">
+            <div class="col-lg-2 ml-auto">
+              <div class="row">
+                <div class="col-lg-4">
+                  <i class="devicon-python-plain big-icon font-cog"></i>
+                </div>
+                <div class="col-lg-4">
+                  <b>Python</b>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mr-auto">
+              <div class="progress progress-bar-custom-back">
+                <div class="progress-bar progress-bar-striped active progress-bar-custom" role="progressbar"
+                aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width:50%;height:30px;">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="row">
+            <div class="col-lg-2 ml-auto">
+              <div class="row">
+                <div class="col-lg-4">
+                  <i class="devicon-java-plain big-icon font-cog"></i>
+                </div>
+                <div class="col-lg-4">
+                  <b>Web Development</b>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mr-auto">
+              <div class="progress progress-bar-custom-back">
+                <div class="progress-bar progress-bar-striped active progress-bar-custom" role="progressbar"
+                aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%;height:30px;">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+
+          <div class="row">
+            <div class="col-lg-2 ml-auto">
+              <div class="row">
+                <div class="col-lg-4">
+                <!--  <i class="fa fa-line-chart big-icon font-cog"></i>-->
+                </div>
+                <div class="col-lg-4">
+                  <b>Machine Learning</b>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mr-auto">
+              <div class="progress progress-bar-custom-back">
+                <div class="progress-bar progress-bar-striped active progress-bar-custom" role="progressbar"
+                aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:10%;height:30px;">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+
+          <div class="row">
+            <div class="col-lg-2 ml-auto">
+              <div class="row">
+                <div class="col-lg-4">
+                  <i class="devicon-android-plain big-icon font-cog"></i>
+                </div>
+                <div class="col-lg-4">
+                  <b>Android</b>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 mr-auto">
+              <div class="progress progress-bar-custom-back">
+                <div class="progress-bar progress-bar-striped active progress-bar-custom" role="progressbar"
+                aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width:5%;height:30px;">
+              </div></div></div></div><br><br>
+    </section>
+
+<!-- projects section -->
+   <section id="Projects" style="background-color:#565b70;color:#ffefc8">
+     <div class="container">
+       <h2 class="text-center" style="font-size:60px;">PROJECTS</h2>
+       <hr>
+      <div class="row">
+         <div class="col-lg-4 ml-auto">
+          <p>I'M Currently Under Graduate In <br>B.tech Information Technology <br>At Cochin University Of <br>Science And Technology          </p>
+         </div>
+         <div class="col-lg-4 mr-auto">
+
+           <p>| Programmer<br>| Web Developer<br> | #Code Python<br> | Beginner in Android Dev.
+           </p>
+         </div>
+       </div>
+     </div>
+   </section>	
+<section id="contact" style="background-color:#18BC9C">
+  <div class="container-grid">
+    <h2 class="text-center" style="font-size:60px;">CONTACT</h2><hr>
+        <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <div class="row"><h3>Shani Kumar Pathak</h3></div>
+            <br>
+            <h5>
+              <i class="fa fa-fw fa-envelope"></i>
+              <a href="mailto:shani.pathak98@gmail.com"> shani.pathak98@gmail.com.com</a>
+            </h5>
+            <h5>
+              <i class="fa fa-fw fa-twitter"></i>
+              <a href="https://twitter.com/ShaniPathak98"> shani kumar</a>
+            </h5>
+            <h5>
+              <i class="fa fa-fw fa-phone"></i>
+              <a href="">+919495040346</a>
+            </h5><br>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+<footer  class="foot" style="background-color:#2C3E50;color:#ffffff">
+       <h5><div class="container">
+         <div class="row ">
+           <div class="footer-col col-md-6">
+             <h3 style="color:#18BC9C">Location</h3>
+             <p><i class="fa fa-map-marker" aria-hidden="true"></i>kochi, kerala , India</p>
+           </div>
+           <div class="footer-col col-md-6 foot">
+          <ul class="list-inline">
+               <li class="list-inline-item">
+                 <a class="btn-social btn-outline" href="https://github.com/shani1998">
+                   <i class="fa fa-github"></i>
+                 </a>
+               </li>
+			   <li class="list-inline-item">
+                 <a class="btn-social btn-outline" href="https://www.linkedin.com/in/shani-kumar-1b01b6127/">
+                   <i class="fa fa-fw fa-linkedin"></i>
+                 </a>
+               </li>
+               <li class="list-inline-item">
+                 <a class="btn-social btn-outline" href="https://plus.google.com/114370942801733800910">
+                   <i class="fa fa-fw fa-google-plus"></i>
+                 </a>
+               </li>
+               <li class="list-inline-item">
+                 <a class="btn-social btn-outline" href="https://twitter.com/ShaniPathak98">
+                   <i class="fa fa-fw fa-twitter"></i>
+                 </a>
+               </li>
+               
+             </ul>
+           </div>
+         </div>
+       </div></h5>
+   </footer>
+  </body>
+  </html>
